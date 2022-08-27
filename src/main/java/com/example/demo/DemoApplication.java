e43ae5f9d0168f7289e6829cca4d387c1bf63277
Josh Brookes <43732832+uol-jdb50@users.noreply.github.com>
2022-08-27 12:50:12 +0100

    Added initial navigation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,77 @@ public class DemoApplication {
 	}
 
 	@RequestMapping("/")
-	String sayHello() {
+	public String sayHello() {
 		return "Hello World!";
+	}
+
+	@RequestMapping("/regs")
+	public String regsList() {
+		return "Regulations";
+	}
+
+	@RequestMapping("/regs/edit")
+	public String regsEdit() {
+		return "Edit Regs";
+	}
+
+	@RequestMapping("/decisions")
+	public String decisions() {
+		return "Decisions";
+	}
+
+	@RequestMapping("/decisions/edit")
+	public String decisionsEdit() {
+		return "Edit Decisions";
+	}
+
+	@RequestMapping("/drivers")
+	public String drivers() {
+		return "Drivers";
+	}
+
+	@RequestMapping("/drivers/edit")
+	public String driversEdit() {
+		return "Edit Drivers";
+	}
+
+	@RequestMapping("/leagues")
+	public String leagues() {
+		return "Leagues";
+	}
+
+	@RequestMapping("/leagues/edit")
+	public String leaguesEdit() {
+		return "Edit Leagues";
+	}
+
+	@RequestMapping("/event")
+	public String events() {
+		return "Events";
+	}
+
+	@RequestMapping("/event/edit")
+	public String eventEdit() {
+		return "Edit Event";
+	}
+
+	@RequestMapping("/reports")
+	public String reports() {
+		return "Reports";
+	}
+
+	@RequestMapping("/reports/edit")
+	public String reportEdit() {
+		return "Edit Report";
+	}
+
+	@RequestMapping("/reports/penalty")
+	public String penalty() {
+		return "Penalty";
+	}
+
+	@RequestMapping("/reports/appeal")
+	public String appeal() {
+		return "Appeal";
 	}
 }
